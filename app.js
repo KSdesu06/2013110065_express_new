d91de9207e2ddd963f6e1ed2a69a489a2d544b18
KSdesu06 <sr.kunyanut_st@tni.ac.th>
2023-01-12 23:54:24 +0700

    Workshop A ย้อนหลัง

--- app.js
+++ app.js
@@ -11,11 +11,14 @@ var shopRouter = require('./routes/shop');
 
 var app = express();
 
-mongoose.connect('mongodb+srv://superkat:0N6cauLZwThyGHXp@2013110065-kat.wponooi.mongodb.net/restfulapi?retryWrites=true&w=majority', 
+const config = require('./config/index')
+mongoose.connect(config.MONGODB_URI, 
 {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({
+    limit: '50mb'
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
